644e434f60f10de88075e5cc038e4a7729a0a3d3
Patr1k00 <124563969+Patr1k00@users.noreply.github.com>
2023-11-11 15:38:15 +0100

    test

---
+++ models/engine/file_storage.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            obj_dict = {}
+            for key, value in FileStorage.__objects.items():
+                if cls == value.__class__.__name__:
+                    obj_dict[key] = value
+            return obj_dict
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
+    def save(self):
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r') as f:
+                content = f.read()
+                if len(content) != 0:
+                    obj = json.loads(content)
+                    for key, value in obj.items():
+                        value = eval(value['__class__'])(**value)
+                        FileStorage.new(self, value)
+
+    def delete(self, obj=None):
+        """Method deletes given argument obj"""
+        if obj is not None:
+            storage = self.all()
+            for key, value in storage.items():
+                if value == obj:
+                    del storage[key]
+                    break
+
+    def close(self):
+        """Close the session from filestorage"""
+        self.reload()
